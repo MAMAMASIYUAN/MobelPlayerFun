f397a21d3404853dbd2304852d9f510e87979ae2
MAMAMASIYUAN <30789683+MAMAMASIYUAN@users.noreply.github.com>
2018-09-07 23:55:01 +0800

    Layout of bottom controller, implement Start/Pause, Video SeekBar
    1. Add layout of bottom video media comtroller.
    2. Implement function Start/Pause
    3. Implement function Video SeekBar

--- app/src/main/java/com/example/iori/mobelplayerfun/activity/SystemVideoPlayer.java
+++ app/src/main/java/com/example/iori/mobelplayerfun/activity/SystemVideoPlayer.java
@@ -4,23 +4,146 @@ import android.app.Activity;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
-import android.widget.MediaController;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
 import com.example.iori.mobelplayerfun.R;
+import com.example.iori.mobelplayerfun.utils.Utils;
 
-public class SystemVideoPlayer extends Activity{
+public class SystemVideoPlayer extends Activity implements View.OnClickListener {
+    /**
+     * Update video progress
+     */
+    private static final int PROGRESS = 1;
     private VideoView videoView;
     private Uri uri;
+    private LinearLayout llTop;
+    private TextView tvName;
+    private ImageView ivBattery;
+    private TextView tvSystemTime;
+    private Button btnVoice;
+    private SeekBar seekbarVoice;
+    private Button btnSwitchPlayer;
+    private LinearLayout llBottom;
+    private TextView tvCurrentTime;
+    private SeekBar seekbarVideo;
+    private TextView tvDuration;
+    private Button btnExit;
+    private Button btnVideoPre;
+    private Button btnVideoStartPause;
+    private Button btnVideoNext;
+    private Button btnVideoSwitchScreen;
+
+    private Utils utils;
+
+    /**
+     * Find the Views in the layout<br />
+     * <br />
+     * Auto-created on 2018-09-07 22:25:36 by Android Layout Finder
+     * (http://www.buzzingandroid.com/tools/android-layout-finder)
+     */
+    private void findViews() {
+        setContentView(R.layout.activity_system_video_player);
+        videoView = (VideoView) findViewById(R.id.videoView);
+        llTop = (LinearLayout)findViewById( R.id.ll_top );
+        tvName = (TextView)findViewById( R.id.tv_name );
+        ivBattery = (ImageView)findViewById( R.id.iv_battery );
+        tvSystemTime = (TextView)findViewById( R.id.tv_system_time );
+        btnVoice = (Button)findViewById( R.id.btn_voice );
+        seekbarVoice = (SeekBar)findViewById( R.id.seekbar_voice );
+        btnSwitchPlayer = (Button)findViewById( R.id.btn_switch_player );
+        llBottom = (LinearLayout)findViewById( R.id.ll_bottom );
+        tvCurrentTime = (TextView)findViewById( R.id.tv_current_time );
+        seekbarVideo = (SeekBar)findViewById( R.id.seekbar_video );
+        tvDuration = (TextView)findViewById( R.id.tv_duration );
+        btnExit = (Button)findViewById( R.id.btn_exit );
+        btnVideoPre = (Button)findViewById( R.id.btn_video_pre );
+        btnVideoStartPause = (Button)findViewById( R.id.btn_video_start_pause );
+        btnVideoNext = (Button)findViewById( R.id.btn_video_next );
+        btnVideoSwitchScreen = (Button)findViewById( R.id.btn_video_switch_screen );
+
+
+        btnVoice.setOnClickListener( this );
+        btnSwitchPlayer.setOnClickListener( this );
+        btnExit.setOnClickListener( this );
+        btnVideoPre.setOnClickListener( this );
+        btnVideoStartPause.setOnClickListener( this );
+        btnVideoNext.setOnClickListener( this );
+        btnVideoSwitchScreen.setOnClickListener( this );
+    }
+
+    /**
+     * Handle button click events<br />
+     * <br />
+     * Auto-created on 2018-09-07 22:25:36 by Android Layout Finder
+     * (http://www.buzzingandroid.com/tools/android-layout-finder)
+     */
+    @Override
+    public void onClick(View v) {
+        if ( v == btnVoice ) {
+            // Handle clicks for btnVoice
+        } else if ( v == btnSwitchPlayer ) {
+            // Handle clicks for btnSwitchPlayer
+        } else if ( v == btnExit ) {
+            // Handle clicks for btnExit
+        } else if ( v == btnVideoPre ) {
+            // Handle clicks for btnVideoPre
+        } else if ( v == btnVideoStartPause ) {
+            // Handle clicks for btnVideoStartPause
+            if(videoView.isPlaying()){
+                //Video is playing, set to pause
+                videoView.pause();
+                //Set button to play
+                btnVideoStartPause.setBackgroundResource(R.drawable.btn_video_start_selector);
+            }else{
+                //Video is pause, set to play
+                videoView.start();
+                //Set button to pause
+                btnVideoStartPause.setBackgroundResource(R.drawable.btn_video_pause_selector);
+
+            }
+        } else if ( v == btnVideoNext ) {
+            // Handle clicks for btnVideoNext
+        } else if ( v == btnVideoSwitchScreen ) {
+            // Handle clicks for btnVideoSwitchScreen
+        }
+    }
+
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what){
+                case PROGRESS:
+                    //1.Get current progress
+                    int currentPosition = videoView.getCurrentPosition();
+                    //2. SeekBar.setProgress(current);
+                    seekbarVideo.setProgress(currentPosition);
+                    tvCurrentTime.setText(utils.stringForTime(currentPosition));
+                    //3. Update the progress every 1 second
+                    handler.removeMessages(PROGRESS);
+                    handler.sendEmptyMessageDelayed(PROGRESS, 1000);
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_system_video_player);
+        utils = new Utils();
+        findViews();
 
-        videoView = (VideoView) findViewById(R.id.videoView);
 
         //准备好播放的监听
         videoView.setOnPreparedListener(new MyOnPreparedListener());
@@ -42,11 +165,24 @@ public class SystemVideoPlayer extends Activity{
 
 
     }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
     class MyOnPreparedListener implements MediaPlayer.OnPreparedListener{
 
         @Override
         public void onPrepared(MediaPlayer mediaPlayer) {
             videoView.start();//Start playing
+            //Get duration
+            int duration = videoView.getDuration();
+            seekbarVideo.setMax(duration);
+            tvDuration.setText(utils.stringForTime(duration));
+            //Send message
+            handler.sendEmptyMessage(PROGRESS);
+
         }
     }
 
