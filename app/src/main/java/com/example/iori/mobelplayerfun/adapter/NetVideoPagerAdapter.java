309826558b674667ddaab494e06ae7e0124125a2
MAMAMASIYUAN <30789683+MAMAMASIYUAN@users.noreply.github.com>
2018-10-06 23:50:22 +0800

    Use Glide and Picasso to request image from net

--- app/src/main/java/com/example/iori/mobelplayerfun/adapter/NetVideoPagerAdapter.java
+++ app/src/main/java/com/example/iori/mobelplayerfun/adapter/NetVideoPagerAdapter.java
@@ -1,16 +1,17 @@
 package com.example.iori.mobelplayerfun.adapter;
 
 import android.content.Context;
-import android.text.format.Formatter;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.example.iori.mobelplayerfun.R;
 import com.example.iori.mobelplayerfun.domain.MediaItem;
-import com.example.iori.mobelplayerfun.utils.Utils;
+import com.squareup.picasso.Picasso;
 
 import org.xutils.x;
 
@@ -61,7 +62,24 @@ public class NetVideoPagerAdapter extends BaseAdapter {
         MediaItem mediaItem = mediaItems.get(position);
         viewHolder.tv_name.setText(mediaItem.getName());
         viewHolder.tv_desc.setText(mediaItem.getDesc());
-        x.image().bind(viewHolder.iv_icon, mediaItem.getImageUrl());
+        //Use xUtils3
+//        x.image().bind(viewHolder.iv_icon, mediaItem.getImageUrl());
+        //Use Glide
+//        Glide.with(context)
+//                .load(mediaItem.getImageUrl())
+//                .diskCacheStrategy(DiskCacheStrategy.ALL)
+//                .placeholder(R.drawable.video_default)
+//                .error(R.drawable.video_default)
+//                .into(viewHolder.iv_icon);
+
+        //Use Picasso
+        Picasso.with(context)
+                .load(mediaItem.getImageUrl())
+                .placeholder(R.drawable.video_default)
+                .error(R.drawable.video_default)
+                .into(viewHolder.iv_icon);
+
+
         return convertView;
     }
 
